1aea709d1d4af44aebcb89b3ce4bec8ac9f1ec77
Vinh Le <lethanhvinh95@gmail.com>
2021-03-26 06:16:21 +0200

    add more tests to see how vulnerable Enzyme tests are

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,46 +1,19 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom'
+import {screen} from '@testing-library/dom'
+import {render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import Counter from '../components/Counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-    const div = document.createElement('div')
-    document.body.append(div)
-    ReactDOM.render(<Counter />, div)
-    const [increment, decrement] = div.querySelectorAll('button')
-    const messageEl = div.firstChild.querySelector('div')
+    render(<Counter />)
+    const increment = screen.getByRole('button', {name: /increment/i})
+    const decrement = screen.getByRole('button', {name: /decrement/i})
+    const message = screen.getByText(/current count/i)
 
-    expect(messageEl.textContent).toBe('Current count: 0')
-
-    increment.click()
-    expect(messageEl.textContent).toContain('1')
-
-    decrement.click()
-    expect(messageEl.textContent).toContain('0')
-
-    div.remove()
+    expect(message).toHaveTextContent(/current count: 0/i)
+    userEvent.click(increment)
+    expect(message).toHaveTextContent(/current count: 1/i)
+    userEvent.click(decrement)
+    expect(message).toHaveTextContent(/current count: 0/i)
 })
-
-test('counter increments and decrements when the buttons are clicked using dispatchEvent', () => {
-    const div = document.createElement('div')
-    document.body.append(div)
-    ReactDOM.render(<Counter />, div)
-    const [increment, decrement] = div.querySelectorAll('button')
-    const messageEl = div.firstChild.querySelector('div')
-    const clickEvent = new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-        button: 0
-    })
-
-    expect(messageEl.textContent).toBe('Current count: 0')
-
-    increment.dispatchEvent(clickEvent)
-    expect(messageEl.textContent).toContain('1')
-
-    decrement.dispatchEvent(clickEvent)
-    expect(messageEl.textContent).toContain('0')
-
-    div.remove()
-})
-
-/* eslint no-unused-vars:0 */
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,11 +4,15 @@ function Counter() {
     const [count, setCount] = React.useState(0)
     const increment = () => setCount(c => c + 1)
     const decrement = () => setCount(c => c - 1)
+    const reset = () => setCount(0)
+
     return (
         <div>
+            <div><h1>My beautiful counter</h1></div>
             <div>Current count: {count}</div>
-            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
             <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
