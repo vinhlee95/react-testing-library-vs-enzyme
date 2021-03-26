d89b9427ba3b918257de7855defebd09f2042e9f
Vinh Le <lethanhvinh95@gmail.com>
2021-03-26 06:04:31 +0200

    add more test configs so that we could run enzyme

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: ['<rootDir>/enzyme.config.js'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+}
