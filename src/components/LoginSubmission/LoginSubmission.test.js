2a2ee1958b57773d56d37f61a42a534ce1ad6d89
Vinh Le <lethanhvinh95@gmail.com>
2021-03-29 06:44:33 +0300

    assert error message to match snapshot

--- src/components/LoginSubmission/LoginSubmission.test.js
+++ src/components/LoginSubmission/LoginSubmission.test.js
@@ -1,45 +1,56 @@
-import * as React from 'react'
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {build, fake} from '@jackfranklin/test-data-bot'
+import * as React from "react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { build, fake } from "@jackfranklin/test-data-bot";
 
-import Login from './LoginSubmission'
+import Login from "./LoginSubmission";
 
 const buildLoginForm = build({
-    fields: {
-        username: fake(f => f.internet.userName()),
-        password: fake(f => f.internet.password()),
-    },
-})
+  fields: {
+    username: fake((f) => f.internet.userName()),
+    password: fake((f) => f.internet.password()),
+  },
+});
 
-test('logging in displays the user\'s username', async () => {
-    render(<Login />)
-    const {username, password} = buildLoginForm()
+test("logging in displays the user's username", async () => {
+  render(<Login />);
+  const { username, password } = buildLoginForm();
 
-    userEvent.type(screen.getByLabelText(/username/i), username)
-    userEvent.type(screen.getByLabelText(/password/i), password)
-    userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  userEvent.type(screen.getByLabelText(/username/i), username);
+  userEvent.type(screen.getByLabelText(/password/i), password);
+  userEvent.click(screen.getByRole("button", { name: /submit/i }));
 
-    await waitForElementToBeRemoved(screen.getByLabelText(/loading/i))
-    expect(screen.getByText(username)).toBeInTheDocument()
-})
+  await waitForElementToBeRemoved(screen.getByLabelText(/loading/i));
+  expect(screen.getByText(username)).toBeInTheDocument();
+});
 
-describe('logging in without username or password should fail', () => {
-    const formData = buildLoginForm()
+describe("logging in without username or password should fail", () => {
+  const formData = buildLoginForm();
 
-    it('should fail without username', async () => {
-        render(<Login />)
-        userEvent.type(screen.getByLabelText(/password/i), formData.password)
-        userEvent.click(screen.getByRole('button', {name: /submit/i}))
-        await waitForElementToBeRemoved(screen.getByLabelText(/loading/i))
-        expect(screen.getByRole('alert')).toHaveTextContent('username is required')
-    })
+  it("should fail without username", async () => {
+    render(<Login />);
+    userEvent.type(screen.getByLabelText(/password/i), formData.password);
+    userEvent.click(screen.getByRole("button", { name: /submit/i }));
+    await waitForElementToBeRemoved(screen.getByLabelText(/loading/i));
+    expect(screen.getByRole("alert")).toMatchInlineSnapshot(`
+      <div
+        role="alert"
+        style="color: red;"
+      >
+        username is required
+      </div>
+    `);
+  });
 
-    it('should fail without password', async () => {
-        render(<Login />)
-        userEvent.type(screen.getByLabelText(/username/i), formData.username)
-        userEvent.click(screen.getByRole('button', {name: /submit/i}))
-        await waitForElementToBeRemoved(screen.getByLabelText(/loading/i))
-        expect(screen.getByRole('alert')).toHaveTextContent('password is required')
-    })
-})
+  it("should fail without password", async () => {
+    render(<Login />);
+    userEvent.type(screen.getByLabelText(/username/i), formData.username);
+    userEvent.click(screen.getByRole("button", { name: /submit/i }));
+    await waitForElementToBeRemoved(screen.getByLabelText(/loading/i));
+    expect(screen.getByRole("alert")).toHaveTextContent("password is required");
+  });
+});
