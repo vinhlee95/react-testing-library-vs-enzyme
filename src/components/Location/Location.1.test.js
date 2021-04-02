6251d2b2e2e3fb7e7f2051d0b9fd946680c1150d
Vinh Le <lethanhvinh95@gmail.com>
2021-04-02 17:37:01 +0300

    test unhappy case when there is an error

--- src/components/Location/Location.1.test.js
+++ src/components/Location/Location.1.test.js
@@ -10,33 +10,69 @@ import Location from './Location'
 
 jest.mock('react-use-geolocation')
 
-test('displays the users current location', () => {
-  // https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition
-  const fakePosition = {
-    coords: {
-      latitude: 0.1,
-      longitude: 0.2
+describe('Location component', () => {
+  it('should display loading spinner', () => {
+    const useMockCurrentPosition = () => {
+      const [state] = React.useState([])
+      return state
     }
-  }
+    useCurrentPosition.mockImplementation(useMockCurrentPosition)
 
-  // State setter
-  let setCurrentPosition
-  const error = null
-  const useMockCurrentPosition = () => {
-    const [initialState, setState] = React.useState([null, error])
-    setCurrentPosition = setState
-    return initialState
-  }
-  useCurrentPosition.mockImplementation(useMockCurrentPosition)
-
-  render(<Location />)
-  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
-
-  act(() => {
-    setCurrentPosition([fakePosition, error])
+    render(<Location />)
+    expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
   })
 
-  expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
-  expect(screen.getByText(/latitude/i)).toHaveTextContent(`Latitude: ${fakePosition.coords.latitude}`)
-  expect(screen.getByText(/longitude/i)).toHaveTextContent(`Longitude: ${fakePosition.coords.longitude}`)
+  it('should display the users current location', () => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition
+    const fakePosition = {
+      coords: {
+        latitude: 0.1,
+        longitude: 0.2
+      }
+    }
+    // State setter
+    let setCurrentPosition
+    const error = null
+    const useMockCurrentPosition = () => {
+      const [initialState, setState] = React.useState([null, error])
+      setCurrentPosition = setState
+      return initialState
+    }
+    useCurrentPosition.mockImplementation(useMockCurrentPosition)
+
+    render(<Location />)
+
+    act(() => {
+      setCurrentPosition([fakePosition, error])
+    })
+
+    expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/latitude/i)).toHaveTextContent(`Latitude: ${fakePosition.coords.latitude}`)
+    expect(screen.getByText(/longitude/i)).toHaveTextContent(`Longitude: ${fakePosition.coords.longitude}`)
+  })
+
+  it('should display an error', () => {
+    let setCurrentPosition
+    const useMockCurrentPosition = () => {
+      const [initialState, setState] = React.useState([null, error])
+      setCurrentPosition = setState
+      return initialState
+    }
+    useCurrentPosition.mockImplementation(useMockCurrentPosition)
+
+    render(<Location />)
+
+    const error = {message: 'No permission'}
+    act(() => {
+      setCurrentPosition([null, error])
+    })
+
+    expect(screen.getByRole('alert')).toHaveTextContent(error.message)
+
+    // We don't want react testing lib to throw an error if the element is not found
+    // https://testing-library.com/docs/queries/about/#types-of-queries
+    expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/latitude/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/longitude/i)).not.toBeInTheDocument()
+  })
 })
