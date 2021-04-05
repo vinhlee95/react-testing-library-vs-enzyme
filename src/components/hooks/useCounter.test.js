82d66783dddafcebd0350a1d0bd3b58de457c804
Vinh Le <lethanhvinh95@gmail.com>
2021-04-05 13:51:45 +0300

    test custom hook by custom setup function which extra hook data by rendering a dummy component

--- src/components/hooks/useCounter.test.js
+++ src/components/hooks/useCounter.test.js
@@ -1,7 +1,11 @@
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from './useCounter'
+
+//
+// 1️⃣ solution: test the hook by testing the component consuming it
+//
 
 // Simple Counter component using the useCounter hook
 const Counter = () => {
@@ -15,6 +19,7 @@ const Counter = () => {
   )
 }
 
+// Test the component that consume useCounter hook
 test('exposes the count and increment/decrement functions', () => {
   render(<Counter />)
   const increment = screen.getByRole('button', {name: /increment/i})
@@ -26,4 +31,53 @@ test('exposes the count and increment/decrement functions', () => {
   expect(message).toHaveTextContent(/current count: 1/i)
   userEvent.click(decrement)
   expect(message).toHaveTextContent(/current count: 0/i)
+})
+
+//
+// -----------------------------
+//
+
+//
+// 2️⃣ solution: test the inner hook logic
+//
+
+/**
+ * Setup function that bind useCounter to a fake component
+ * This way, when we test the logic inside the hook, the state inside the hook also get updated
+ *
+ * @param options
+ * @returns "reference" to the hook logic
+ */
+function setup({...options} = {}) {
+  const result = {}
+  const TestComponent = () => {
+    // Update the result variable in-place so that its reference (hookData variable in this case)
+    // could access updated state when it changed
+    // since the reference still point to the same object
+    result.current = useCounter({initialCount: 0, step: 1, ...options})
+    return null
+  }
+  render(<TestComponent />)
+  return result
+}
+
+test('useCounter hook allow to update the counter', () => {
+  const hookData = setup()
+  // Assert initial state
+  expect(hookData.current.count).toEqual(0)
+
+  act(() => {
+    hookData.current.increment()
+  })
+  expect(hookData.current.count).toEqual(1)
+})
+
+test('useCounter hook allow customization of the initial count and step', () => {
+  const hookData = setup({initialCount: 2, step: 2})
+  // Assert initial state
+  expect(hookData.current.count).toEqual(2)
+  act(() => {
+    hookData.current.increment()
+  })
+  expect(hookData.current.count).toEqual(4)
 })
